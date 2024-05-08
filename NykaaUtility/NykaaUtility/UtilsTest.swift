6de1ec146c2250bbeb42bf217c81d36c88e16943
Pravin Sankeshwari <pravin.sankeshwari@nykaa.com>
2024-05-08 17:45:39 +0530

    added new function

---
+++ NykaaUtility/NykaaUtility/UtilsTest.swift
@@ -0,0 +1,14 @@
+//
+//  UtilsTest.swift
+//  NykaaUtility
+//
+//  Created by Pravin Sankeshwari on 08/05/24.
+//
+
+import Foundation
+
+public class UtilsTest {
+    public func featureOne() {
+        print("feature one")
+    }
+}
